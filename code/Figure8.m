16397775d7b8df9b9c39b619e96415ee33b002c1
AhmedFahmySoliman <82141384+AhmedFahmySoliman@users.noreply.github.com>
2026-06-30 02:19:01 +0100

    Update Figure8.m

--- code/Figure8.m
+++ code/Figure8.m
@@ -1,46 +1,31 @@
 function Figure8()
 
-load('../data/CompactData/Hardware_Compact_All.mat','Compact')
+load('../data/CompactData/Figure8_Stats.mat','Stats')
 
-Data_Prop = Compact.Prop;
-Data_RSDP = Compact.RSDP;
+t = Stats.Figure8.t;
 
-l = length(Data_Prop);
-t = Compact.Time{1};
+ErrMean_Prop = Stats.Figure8.ErrMean_Prop;
+ErrStd_Prop  = Stats.Figure8.ErrStd_Prop;
 
-for i = 1:l
-    Err_Prop_All(:,:,i) = Data_Prop(i).NTau_est - ...
-                          Data_Prop(i).NTau_act;
+ErrMean_RSDP = Stats.Figure8.ErrMean_RSDP;
+ErrStd_RSDP  = Stats.Figure8.ErrStd_RSDP;
 
-    Err_RSDP_All(:,:,i) = Data_RSDP(i).NTau_est - ...
-                          Data_RSDP(i).NTau_act;
+CTime_Prop_m = Stats.Figure8.CTime_Prop_m;
+CTime_Prop_s = Stats.Figure8.CTime_Prop_s;
 
-    CTime_Prop(:,i) = Data_Prop(i).Comp_Time_ms;
-    CTime_RSDP(:,i) = Data_RSDP(i).Comp_Time_ms;
-end
-Err_Prop_m = mean(abs(Err_Prop_All),2);
-Err_RSDP_m = mean(abs(Err_RSDP_All),2);
+CTime_RSDP_m = Stats.Figure8.CTime_RSDP_m;
+CTime_RSDP_s = Stats.Figure8.CTime_RSDP_s;
 
-Err_Prop_m = squeeze(Err_Prop_m);
-Err_RSDP_m = squeeze(Err_RSDP_m);
+X_SP = [0 13];
 
-CTime_Prop_m = mean(CTime_Prop,2,'omitnan');
-CTime_RSDP_m = mean(CTime_RSDP,2,'omitnan');
+C = [0.98,0.71,0.82;
+     0.6,1,0;
+     0.5,0.85,1;
+     0.8,0.8,0.8];
 
-CTime_Prop_s = std(CTime_Prop,0,2,'omitnan');
-CTime_RSDP_s = std(CTime_RSDP,0,2,'omitnan');
+figure(8)
+clf
 
-% ---------- Hardware ----------
-ErrMean_Prop = mean(Err_Prop_m,2,'omitnan');
-ErrStd_Prop  = std(Err_Prop_m,0,2,'omitnan');
-
-ErrMean_RSDP = mean(Err_RSDP_m,2,'omitnan');
-ErrStd_RSDP  = std(Err_RSDP_m,0,2,'omitnan');
-
-
-X_SP = [0 13]; 
-C = [0.98,0.71,0.82;0.6,1,0;0.5,0.85,1;0.8,0.8,0.8];
-figure(12)
 subplot(2,1,1)
 hold on
 plot_patch_std(t, ErrMean_Prop, ErrStd_Prop, C(1,:));
@@ -48,13 +33,16 @@ plot_patch_std(t, ErrMean_RSDP, ErrStd_RSDP, C(2,:));
 
 p1 = plot(t,ErrMean_Prop,'r-','LineWidth',2);
 p2 = plot(t,ErrMean_RSDP,'Color',[0.0,0.6,0],'LineWidth',2);
-legend([p1 p2],'Proposed','RSDP','Location','northeast','Numcolumns',1);
-grid on, box on
- 
+
+legend([p1 p2],'Proposed','RSDP','Location','northeast','NumColumns',1);
+grid on
+box on
+
 xlim(X_SP);
 ylabel('e_{\tau} [N.m]')
-set(gca,'Xticklabel',[]);
-set(gca,'FontName', 'Times New Roman','FontSize', 12,'LineWidth',1)
+set(gca,'XTickLabel',[]);
+set(gca,'FontName','Times New Roman','FontSize',12,'LineWidth',1)
+
 a_pos = get(gca,'Position');
 set(gca,'Position',[a_pos(1) a_pos(2)-0.02 a_pos(3)+0.05 a_pos(4)+0.07])
 
@@ -65,15 +53,18 @@ plot_patch_std(t, CTime_RSDP_m, CTime_RSDP_s, C(2,:));
 
 p1 = plot(t,CTime_Prop_m,'r-','LineWidth',2);
 p2 = plot(t,CTime_RSDP_m,'Color',[0.0,0.6,0],'LineWidth',2);
-legend([p1 p2],'Proposed','RSDP','Location','northeast','Numcolumns',1);
-grid on, box on
- 
+
+legend([p1 p2],'Proposed','RSDP','Location','northeast','NumColumns',1);
+grid on
+box on
+
 xlim(X_SP);
 ylabel('t_c [ms]')
 xlabel('Time [s]')
-set(gca,'FontName', 'Times New Roman','FontSize', 12,'LineWidth',1)
+set(gca,'FontName','Times New Roman','FontSize',12,'LineWidth',1)
+
 a_pos = get(gca,'Position');
-set(gca,'Position',[a_pos(1) a_pos(2)-0.0 a_pos(3)+0.05 a_pos(4)+0.07])
+set(gca,'Position',[a_pos(1) a_pos(2) a_pos(3)+0.05 a_pos(4)+0.07])
 
 saveCurrentFigure('Figure8');
 
@@ -82,4 +73,5 @@ disp(' Figure saved successfully!');
 disp(' Please, display the results by left clicking');
 disp('  "Figure8.png" in the "results" folder.');
 disp('====================================================');
+
 end
